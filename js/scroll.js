6a9f84bd94b28cb0fba762fe3e115496664cb9d8
Gabriel Henrique <smartsbiel@gmail.com>
2023-05-04 08:26:31 -0300

    first-commit

---
+++ js/scroll.js
@@ -0,0 +1,6 @@
+window.sr = ScrollReveal({ reset:true});
+ScrollReveal({ distance: '35px' });
+sr.reveal('.about-us', {duration : 3000, origin: 'bottom'})
+sr.reveal('.beneficios', {duration : 3000, origin: 'bottom'})
+sr.reveal('.places-cards', {duration : 3000,  origin: 'bottom'})
+sr.reveal('.footer', {duration : 3000,   origin:  'bottom'})
